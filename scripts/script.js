bd40cb835ef5ad4b10095d8b31c8b24fd0d39070
Tibi <tvoinea@my.bcit.com>
2022-11-30 23:33:55 -0800

    added functionality to the new note button to append to the bottom of the Li element

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,18 @@ const article = document.querySelector('.section');
 const newnotebutton = document.querySelector('.newnote');
 const cancelButton = document.querySelector('.cancelbutton');
 const saveButton = document.querySelector('.savebutton');
-const textArea = document.querySelector('#textbox');
+const textArea = document.querySelector('.textbox');
+const emptyArray = [];
+const noteList = document.querySelector('.listNotes');
+
+
 
 function changeTheme() {
     darkthemebutton.classList.toggle('darkthemebuttondark');
     aside.classList.toggle('lightthemeaside');
     article.classList.toggle('lightthemearticle');
     newnotebutton.classList.toggle('darkactionbutton');
+    textArea.classList.toggle('lightthemetextarea');
     if (darkthemebutton.textContent == 'Light Theme') {
         darkthemebutton.textContent = 'Dark Theme';
     }
@@ -41,3 +46,26 @@ function newNoteButton() {
 }
 
 newnotebutton.addEventListener('click', newNoteButton);
+
+function appendNewNote() {
+    const listItems = document.createElement('li');
+        const newNote = prompt('Enter a new note');
+        {
+            emptyArray.push({title: newNote, body: textArea.value});
+        }
+        listItems.appendChild(document.createTextNode(newNote));
+        noteList.appendChild(listItems);
+}
+
+saveButton.addEventListener('click', appendNewNote);
+
+function populateTextArea(event) {
+        for (let i = 0; i < emptyArray.length; i++) {
+            if (event.target.textContent === emptyArray[i].title) {
+                textArea.value = emptyArray[i].body;
+            }
+        }
+
+}
+
+noteList.addEventListener('click', populateTextArea);
